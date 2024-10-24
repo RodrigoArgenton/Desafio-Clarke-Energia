5595e04908d9c37c8c45e4146bf7397997cb3d73
Rodrigo Argenton Barbosa <rodrigolk4321@gmail.com>
2024-10-23 21:40:54 -0300

    metodo get para listar todos os itens

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
 
 def create_app():
     app = Flask(__name__)
 
+    def load_data():
+        with open('app/data/supplier.json', 'r', encoding='utf-8') as file:
+            supplier = json.load(file)
+        return supplier
+
     @app.route('/')
     def home():
         return {"message": "Api de fonecedores"}
-    
+
+    @app.route('/supplier', methods=['GET'])
+    def list_supplier():
+        supplier = load_data()
+        return jsonify(supplier)
+
     return app
